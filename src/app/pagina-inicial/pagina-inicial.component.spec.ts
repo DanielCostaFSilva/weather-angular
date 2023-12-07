6d73895af3f4aad2072b06edbc4f3227950346a0
Danielcosta <danielcfsilva@gmail.com>
2023-12-07 12:37:15 -0300

    inclusao do teste unitario do componete pagina-inicial

--- src/app/pagina-inicial/pagina-inicial.component.spec.ts
+++ src/app/pagina-inicial/pagina-inicial.component.spec.ts
@@ -1,21 +1,59 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';  // Adicione esta importação
 import { PaginaInicialComponent } from './pagina-inicial.component';
+import { WeatherService } from '../weather.service';
+import { DataSharingService } from '../data-sharing.service';
+import { of } from 'rxjs';
+
+class MockWeatherService {
+  getWeather(cidade: string) {
+    return of(/* dados simulados */);
+  }
+}
+
+class MockDataSharingService {
+  setWeatherData(data: any) {
+    // Implemente conforme necessário
+  }
+}
 
 describe('PaginaInicialComponent', () => {
   let component: PaginaInicialComponent;
   let fixture: ComponentFixture<PaginaInicialComponent>;
+  let mockWeatherService: MockWeatherService;
+  let mockRouter: Router;
 
   beforeEach(() => {
+    mockWeatherService = new MockWeatherService();
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [PaginaInicialComponent]
+      declarations: [PaginaInicialComponent],
+      imports: [RouterTestingModule], // Certifique-se de importar RouterTestingModule
+      providers: [
+        { provide: WeatherService, useValue: mockWeatherService },
+        { provide: DataSharingService, useClass: MockDataSharingService },
+        { provide: Router, useValue: mockRouter },
+      ],
     });
+
     fixture = TestBed.createComponent(PaginaInicialComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getWeather and navigate when buscarDetalhes is called', () => {
+    const cidade = 'Dallol';
+
+    spyOn(mockWeatherService, 'getWeather').and.returnValue(of(/* dados simulados */));
+
+    component.buscarDetalhes(cidade);
+
+    expect(mockWeatherService.getWeather).toHaveBeenCalledWith(cidade);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/detalhes', cidade]);
   });
 });
