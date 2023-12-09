efcecdb19d37f2304c2a5a0b15c9024600c84e16
Danielcosta <danielcfsilva@gmail.com>
2023-12-09 14:04:47 -0300

    criacao de interfaces para buscar pegar so os dados que preciso

--- src/app/detalhes-cidade/detalhes-cidade.component.ts
+++ src/app/detalhes-cidade/detalhes-cidade.component.ts
@@ -3,6 +3,17 @@ import { ActivatedRoute } from '@angular/router';
 import { DataSharingService } from '../data-sharing.service';
 import { WeatherService } from '../weather.service';
 
+interface WeatherData {
+  main: { temp: number; humidity: number };
+  sys: { sunset: number; sunrise: number };
+  wind: { speed: number };
+  weather: { description: string; icon: string }[];
+}
+
+interface ForecastData {
+  list: { main: { temp: number }; weather: { icon: string }[] }[];
+}
+
 @Component({
   selector: 'app-detalhes-cidade',
   templateUrl: './detalhes-cidade.component.html',
@@ -10,9 +21,8 @@ import { WeatherService } from '../weather.service';
 })
 export class DetalhesCidadeComponent implements OnInit {
   cidadeSelecionada: string = '';
-  weatherData: any;
-  forecastData: any;
-  forecastDataList: any;
+  weatherData: WeatherData | undefined;
+  forecastDataList: ForecastData['list'] | undefined;
   timeSunset: string = '';
   timeSunrise: string = '';
   horaFormatada: string = '';
@@ -29,9 +39,8 @@ export class DetalhesCidadeComponent implements OnInit {
   ngOnInit(): void {
     this.cidadeSelecionada = this.route.snapshot.params['cidade'];
 
-    this.dataSharingService.weatherData$.subscribe((data) => {
+    this.dataSharingService.weatherData$.subscribe((data: WeatherData) => {
       this.weatherData = data;
-
       this.timeSunset = this.formatarHoraMinuto(data.sys.sunset);
       this.timeSunrise = this.formatarHoraMinuto(data.sys.sunrise);
       this.temp = Math.floor(data.main.temp);
@@ -39,23 +48,17 @@ export class DetalhesCidadeComponent implements OnInit {
 
     this.weatherService
       .getWeatherForecast(this.cidadeSelecionada)
-      .subscribe((forecast) => {
-        this.forecastData = forecast;
+      .subscribe((forecast: ForecastData) => {
         this.forecastDataList = forecast.list;
       });
   }
 
   formatarHoraMinuto(timestemp: number): string {
     const data = new Date(timestemp * 1000);
-
     const horas = data.getHours();
     const minutos = data.getMinutes();
     const periodo = horas >= 12 ? 'PM' : 'AM';
-
-    // Converte para formato de 12 horas
     const horasFormatadas = horas % 12 || 12;
-
-    // Adiciona um zero à esquerda para minutos menores que 10
     const minutosFormatados = minutos < 10 ? `0${minutos}` : minutos;
     this.horaFormatada = `${horasFormatadas}:${minutosFormatados} ${periodo}`;
     return this.horaFormatada;
@@ -67,12 +70,10 @@ export class DetalhesCidadeComponent implements OnInit {
 
   convertToTemperature(value: any): string {
     const convertedValue = +value;
-
     if (!isNaN(convertedValue)) {
       const roundedValue = convertedValue.toFixed(0);
       return `${roundedValue}°C`;
     }
-
     return 'Erro de conversão';
   }
 }
